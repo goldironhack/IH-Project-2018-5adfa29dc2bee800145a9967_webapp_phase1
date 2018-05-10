078a53123de4c005f71d61f23cb55966f285ec90
jdbarraganm <jdbarraganm@unal.edu.co>
2018-05-09 23:43:41 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 ## webapp_phase1
 
 undefined
+
+1. Find me a place.
+2. Keywords: Safety, distance, affordability, maps.
+3. In this dataset I bring information about the neighborhoods like name, position and borough
+Datasets used: NEIGHBORHOOD_NAMES = "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD";[10][9][16]
+THe geoJson I used it to draw the districts.
+
+GeoJson: "http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"
+4.in this first itetarion of the project I put a map and drew in it the 60 habitable districts and put a marker in the map indicating the position of the NYU Stern School of Business. In this version I get data about the neighborhoods like the satelital location(lat,lng) and the borough which it belongs. In this version I addes a list of options about what the app will be inthe future.
+5.Map View:
+
+[YES] Basic Map with specific location:I locate it inthe university.
+[NO] [describe] Any cover on the map (for example, cloud cover to show the weather effect)
+Data Visualization:
+
+[NO] [describe] Use Graph? What is the type? (bar chart, pie chart, radar chart ...)
+[NO] [List] Any interaction available on the graph? List them (enable click on numbers, drag on lines, change time/variables ...)
+Interaction Form:
+
+[YES] [List] Any information output? list them. (text field, text area, label, plain HTML ...): A table.
+[NO] [List] Any operation option (filters)? List them. (search markets, search vegetables, filter based on price, sort based on convenience ...)
+[NO] [List] Any information input? List them. (comments, markers, user preference ...)
+[NO] [List] Interaction with Map? List them. (filter on price will affect map markers, sort on price will affect map markers, ...)
+[YES] [List] Interaction with data visualization? List them. (filter, sort, set variables ...): In a table I show data about the districts.
+
+6.Test Case Which browsers did you test your project on? Chrome, IE, Edge, Safari, or Firefox?
--- index.js
+++ index.js
@@ -1,6 +1,72 @@
+const API_KEY = "AIzaSyBwTL-fCgMYyPAPkpd_vRQfQnBuU_uLbOs";
+const NEIGHBORHOOD_NAMES = "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD";
+var infoRows = [];
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+var map;
+var nyu_coordinates={lat:40.729100, lng: -73.996500}
+var nyu_marker;
+var directionsService;
+var directionsRenderer;
+
+function getData(URL){
+  var data = $.get(NEIGHBORHOOD_NAMES, function(){})
+  .done(function(){
+    var dataR = data.responseJSON.data;
+    for (var i=0;i<dataR.length;i++){
+      infoRows.push([dataR[i][10],dataR[i][9],dataR[i][16]]);
+    }
+    var tableReference = $("#tableBody")[0];
+    var newR,neighborhood,ubication,borough;
+    for(var j=0;j<dataR.length;j++){
+      newR = tableReference.insertRow(tableReference.rows.length);
+      neighborhood = newR.insertCell();
+      ubication = newR.insertCell();
+      borough = newR.insertCell();
+      neighborhood.innerHTML = infoRows[j][0];
+      ubication.innerHTML = infoRows[j][1];
+      borough.innerHTML = infoRows[j][2];
+    }
+    console.log(infoRows);
+  })
+  .fail(function error(){
+    cconsole.log(error);
+  })
+}
+$("document").ready(function (){
+  $("#ShowLocations").on("click",getData)
+})
+
+
+
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: nyu_coordinates
+  });
+  nyu_marker = new google.maps.Marker({
+    position: nyu_coordinates,
+    map: map
+  });
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  //markerEvents(ny_marker);
+  map.data.loadGeoJson('https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
+
+  map.data.setStyle(function(feature) {
+  var color = 'black';
+  if (feature.getProperty('isColorful')) {
+    color = feature.getProperty('color');
+  }
+  return /** @type {google.maps.Data.StyleOptions} */({
+    fillColor: color,
+    strokeColor: color,
+    strokeWeight: 1
+  });
+});
+}
+
+/*function markerEvents(marker){
+  if(marker != "undefined"){
+    marker.addListener("click",)
+  }
+}*/
